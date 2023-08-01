6641936cac124801643e78ed2278999aae5706dc
simran bhandari <simranbhandari036@gmail.com>
2023-08-01 11:44:11 +0530

    Creating schemas and connect to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const {default: mongoose} = require("mongoose");
 
 const app = express();
 dotenv.config();
+
+const connectDB = async () => {
+    try{
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log("Server is connected to DB");
+    } catch(err){
+        console.log("Server is not connected to DB", err.message);
+    }
+};
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("API is running...");
